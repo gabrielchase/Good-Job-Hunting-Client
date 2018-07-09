ec340c5b007ff01463a1640ee38d8e902e9ea393
gabrielchase <gchase.patron@gmail.com>
2018-07-10 00:52:27 +0800

    Create login component

--- src/components/login.js
+++ src/components/login.js
@@ -1,7 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default () => (
-    <div>
-        <h1>Login</h1>
-    </div>
-)
+const mapStateToProps = (state) => {
+    return {
+        auth: state.auth
+    }
+}
+
+class Login extends Component { 
+    constructor() {
+        super()
+
+        this.state = {
+            email: '',
+            password: '',
+            error: false
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+        // this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    componentDidMount() {
+        if (this.props.auth.data) 
+            this.setState({ email: this.props.auth.data.email}) 
+    }
+
+    handleChange(e) {
+        this.setState({ [e.target.id]: e.target.value })
+    }
+
+    render() {
+        const { email, password, error } = this.state
+        return (
+            <div>
+                <h1>Login</h1>
+                <label htmlFor="email">Email</label>
+                <input 
+                    type="text"
+                    id="email"
+                    value={email}
+                    onChange={this.handleChange}
+                />
+                <label htmlFor="password">Password</label>
+                <input 
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={this.handleChange}
+                />
+                <button type="submit">Login</button>
+            </div>
+        )
+    }
+}
+    
+export default connect(mapStateToProps)(Login)
+
