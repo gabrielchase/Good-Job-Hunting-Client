b1366b285d7b300179c8f82302f31d4796173d6b
gabrielchase <gchase.patron@gmail.com>
2018-07-11 21:50:31 +0800

    Fix register to just use email and do login action

--- src/actions/index.js
+++ src/actions/index.js
@@ -20,12 +20,31 @@ export const registerUser = (user) => async dispatch => {
     if (res.data.success) {
         await dispatch({
             type: 'REGISTER_USER_SUCCESS',
-            payload: res.data
+            payload: { email: res.data.data.email }
         })
         return { success: true }
     } else {
         dispatch({
             type: 'REGISTER_USER_FAIL',
+            payload: res.data
+        })
+        return { success: false }
+    }
+}
+
+export const loginUser = (user) => async dispatch => {
+    const headers = getHeaders(false)
+    const res = await axios.post(`${API_URL}/login`, user, headers)
+    localStorage.setItem('user_auth', res.data)
+    if (res.data.success) {
+        await dispatch({
+            type: 'LOGIN_SUCCESS',
+            payload: res.data
+        })
+        return { success: true }
+    } else {
+        dispatch({
+            type: 'LOGIN_FAIL',
             payload: res.data
         })
         return { success: false }
